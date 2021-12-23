67780e1883968467a3321640464735381881698d
YosraAzz <yosra.azzouz@epfl.ch>
2021-12-23 22:25:15 +0100

    YosraAz_Submit

--- include/mockturtle/algorithms/simulation_cec.hpp
+++ include/mockturtle/algorithms/simulation_cec.hpp
@@ -40,6 +40,7 @@
 #include "miter.hpp"
 #include "simulation.hpp"
 
+
 namespace mockturtle
 {
 
@@ -74,12 +75,134 @@ public:
   bool run()
   {
     /* TODO: write your implementation here */
-    return false;
+
+    /*compute split_var and rounds */
+    _st.split_var = compute_splitting_var(_ntk);
+    _st.rounds = compute_rounds(_ntk,_st.split_var);
+
+    /*print split_var and rounds*/
+    std::cout << "split_var : " << _st.split_var << std::endl;
+    std::cout << "num of rounds : " << _st.rounds << std::endl;
+
+    pattern_t patterns(_ntk);
+
+
+    /*initialize  patterns and simulate first round */ 
+    init_patterns( _ntk, _st.split_var,patterns);
+    default_simulator<kitty::dynamic_truth_table> simulator(_st.split_var);
+    simulate_nodes( _ntk, patterns,simulator );
+
+    /*first equivalence check, returning only False if the equivalence check is False */
+    if (!check_eq( _ntk,patterns )){
+      return(false);
+    }
+   
+    /*update and simulate the different rounds */
+    for (uint32_t round = 1; round <= _st.rounds - 1; ++round){
+
+       pattern_clear(patterns);
+       update_pattern(round,patterns);
+       simulate_nodes( _ntk, patterns,simulator );
+
+      /* quit the loop only if a check is False, if it is True we need to iterate through all the simulation rounds*/
+        if (!check_eq( _ntk,patterns )){
+          return(false);
+          }
+     }
+    
+    return true;
+    
   }
 
 private:
   /* you can add additional methods here */
 
+  /* compute split_var based on the mathematical expression  */
+  uint32_t compute_splitting_var(Ntk& _N){
+    uint32_t n,v,k;
+    n=_N.num_pis();   /* number of inputs */
+    v=_N._storage->nodes.size();  /*number of nodes */
+    k= 3+ ( (log((1<<29)/v-32))/log(2) ) ; /*upper limit of m */
+    if (n<=6) {
+      return(n);
+    } 
+    else {
+      return(std::min(k,n));   /* split_var should satisfy both conditions <n and <=k , hence the function min(k,n)  */
+    }
+  }
+
+  /* compute number of rounds of the simulation  */
+  uint32_t compute_rounds(Ntk& _N, uint32_t split_var){
+    uint32_t n=_N.num_pis();
+    return(1 << (n-split_var));
+  }
+
+  void init_patterns( Ntk& _N, uint32_t split_var, pattern_t& patterns){
+
+     /*pattern_t patterns(_N);*/
+     
+     _N.foreach_pi( [&]( auto const& n, auto p ){ 
+      kitty::dynamic_truth_table tt (split_var);
+      if (p < split_var) {
+      kitty:: create_nth_var(tt , p);
+      }
+      patterns[n]=tt;
+    } );
+
+  }
+  
+/* equivalence check function */
+  bool check_eq (Ntk& _N ,pattern_t& patterns){
+    bool equiv_check ;
+    equiv_check=true;
+    _N.foreach_po( [&]( auto const& f) {
+    if ( _N.is_complemented( f ) )  /* the case of complemented function*/
+    {
+      if ( !is_const0(~patterns[f]) ) {  /*checking bit by bit if the outputs are 0 (True) */
+        equiv_check = false;
+      }
+    }
+    else
+    {
+      if ( !is_const0(patterns[f]) ) {
+      equiv_check = false; 
+      }
+    }
+  } );
+  return equiv_check;
+}
+
+/*the function to update the pattern*/
+  void update_pattern( uint32_t& round ,pattern_t& patterns ){
+    uint32_t r = round;  
+
+      _ntk.foreach_pi( [&]( auto const& j, auto k ) 
+      {
+        if (k >= _st.split_var ){
+          if (r % 2 == 1) {
+             if ( is_const0(patterns[j]) ) patterns[j] = ~patterns[j];
+          }
+      
+        else {
+           if ( !is_const0(patterns[j]) ) patterns[j] = ~patterns[j];
+        }
+        r /= 2;
+  
+        }
+      } );
+  }
+
+    /* function to clear the patterns to avoid alloc errors */
+     void pattern_clear( pattern_t& patterns ){
+    _ntk.foreach_gate( [&]( auto const& n )
+    {
+       patterns.erase(n);
+    } );
+  }
+
+//};
+
+  
 private:
   Ntk& _ntk;
   simulation_cec_stats& _st;
